28dbd20640c03b5ac51da10966d4795118271dac
marcoaam <marco.army@gmail.com>
2014-07-07 18:22:53 +0100

    Docking station working

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,14 @@
 class DockingStation
+
+	DEFAULT_CAPACITY = 10
 	
-	def initialize
+	def initialize(options = {})
+		@capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
 		@bikes = []
+	end
+
+	def capacity
+		@capacity
 	end
 
 	def bike_count
@@ -10,5 +17,13 @@ class DockingStation
 
 	def dock(bike)
 		@bikes << bike
+	end
+
+	def release(bike)
+		@bikes.delete(bike)
+	end
+
+	def full?
+		bike_count == @capacity
 	end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,18 +1,28 @@
 require 'docking_station'
 require 'bike'
 
-
-
 describe DockingStation do
 
-	let(:station) { DockingStation.new }
+	let(:station) { DockingStation.new(capacity: 20) }
 	let(:bike) { Bike.new }
 
 	it 'should accept a bike' do
 		expect(station.bike_count).to eq 0
 
 		station.dock(bike)
-		
+
 		expect(station.bike_count).to eq(1)
+	end
+
+	it 'Should release a bike' do
+		station.dock(bike)
+		station.release(bike)
+		expect(station.bike_count).to eq(0)
+	end
+
+	it 'Should know when it is full' do
+		expect(station.full?).to be false
+		(station.capacity).times {station.dock(Bike.new)}
+		expect(station.full?).to be true
 	end
 end
